cdbf7ee22cce7ae37c04f0126194f87e2669c542
Reggierizky <reggierizkyprasetya412@gmail.com>
2025-09-25 22:04:47 +0700

    first commit

---
+++ JavaScript1.js
@@ -0,0 +1,20 @@
+//Reggie Rizky Prasetya TI24G
+
+//1. Deklarasi Variabel
+let matematika = 85;
+let bahasaInggris = 90;
+let ipa = 78;
+
+//Perhitungan Rata-Rata
+let nilaiRataRata = (matematika + bahasaInggris + ipa) / 3;
+   
+//Tipe data dan output
+console.log("Nilai Rata-Rata:", nilaiRataRata);
+console.log("Tipe Data:", typeof nilaiRataRata);
+
+//Kondisi if-else
+if (nilaiRataRata >= 80) {
+    console.log("Status: Lulus");
+} else {
+    console.log("Status: Tidak Lulus");
+}
